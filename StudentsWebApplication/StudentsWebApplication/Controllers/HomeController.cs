0a7fcd2c50d21b36c9ed1895027b10881a56efd6
bravesimon <46894011+bravesimon@users.noreply.github.com>
2023-12-10 20:06:33 +0100

    created project and made db connetction

---
+++ StudentsWebApplication/StudentsWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentsWebApplication.Data;
+using StudentsWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StudentsDbContext _context;
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(StudentsDbContext context)
+        {
+            _context = context;
+        }
+
+      /*  public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }*/
+
+        public IActionResult Index()
+        {
+            var students = _context.Students.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StudentsWebApplication/StudentsWebApplication/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace StudentsWebApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
