4111f1feb2e151792110f73a9841f9f07211e57a
bravesimon <46894011+bravesimon@users.noreply.github.com>
2024-06-06 23:12:08 +0200

    finished complex view for more info

---
+++ MechanicService/MechanicService/Controllers/ComplexController.cs
@@ -0,0 +1,36 @@
+﻿using MechanicService.Data;
+using MechanicService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MechanicService.Controllers
+{
+    public class ComplexController : Controller
+    {
+        private readonly ServiceDbContext _context;
+        
+        public ComplexController(ServiceDbContext context) 
+        {
+            _context = context;
+        }
+
+        public IActionResult ComplexView()
+        {
+            var infos = (from clients in _context.Clients
+                              join vehicles in _context.Vehicles on clients.Id equals vehicles.ClientID
+                              join services in _context.Services on vehicles.Id equals services.VehicleId
+                              select new ComplexVM
+                              {
+                                  FirstName = clients.FirstMidName,
+                                  LastName = clients.LastName,
+                                  VehicleType = vehicles.Type.ToString(),
+                                  IsWarrantyActive = vehicles.IsWarrantyActive,
+                                  Date = services.Date.ToLongDateString(),
+                                  IsDone = services.IsDone
+                              });
+
+            List<ComplexVM> result = [.. infos];
+
+            return View(result);
+        }
+    }
+}
---
+++ MechanicService/MechanicService/Models/ComplexVM.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MechanicService.Models
+{
+    public class ComplexVM
+    {
+        public string FirstName;
+        public string LastName;
+        public string VehicleType;
+        public bool IsWarrantyActive;
+        public string Date;
+        public bool IsDone;
+    }
+}
