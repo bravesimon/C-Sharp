3d7682e7a7d8d1af30157b74ced487d5b0f789a7
bravesimon <46894011+bravesimon@users.noreply.github.com>
2024-06-07 00:10:43 +0200

    added listing services by not done filter

---
+++ MechanicService/MechanicService/Controllers/NotDoneServicesController.cs
@@ -0,0 +1,39 @@
+﻿using MechanicService.Data;
+using MechanicService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MechanicService.Controllers
+{
+    
+    public class NotDoneServicesController : Controller
+    {
+         private readonly ServiceDbContext _context;
+
+        public NotDoneServicesController(ServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult NotDoneServicesView()
+        {
+            var infos = (from clients in _context.Clients
+                         join vehicles in _context.Vehicles on clients.Id equals vehicles.ClientID
+                         join services in _context.Services on vehicles.Id equals services.VehicleId
+                         join addresses in _context.Addresses on clients.AddressId equals addresses.Id
+                         where services.IsDone == false
+                         select new NotDoneServicesVM
+                         {
+                             FirstName = clients.FirstMidName,
+                             LastName = clients.LastName,
+                             Email = clients.Email,
+                             Phone = clients.PhoneNumber,
+                             City = addresses.City,
+                             IsWarrantyActive = vehicles.IsWarrantyActive,
+                             Date = services.Date.ToLongDateString(),
+                         });
+
+            List<NotDoneServicesVM> result = [.. infos];
+            return View(result);
+        }
+    }
+}
---
+++ MechanicService/MechanicService/Models/NotDoneServicesVM.cs
@@ -0,0 +1,13 @@
+﻿namespace MechanicService.Models
+{
+    public class NotDoneServicesVM
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Phone;
+        public string City;
+        public bool IsWarrantyActive;
+        public string Date;
+    }
+}
